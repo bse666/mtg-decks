ad2b553e4a6c84c6c5d644391d2294e830d30aa8
James Faix <jamesthefaix@gmail.com>
2021-03-15 11:30:05 -0700

    Refactored into multiple files

---
+++ utils/ShandalarToCockatrice/Model.cs
@@ -0,0 +1,27 @@
+﻿namespace ShandalarToCockatrice
+{
+    public class DeckItem
+    {
+        public string Name;
+        public int Count;
+    }
+
+    public class Deck
+    {
+        public string Name;
+        public DeckItem[] Cards;
+        public DeckItem[] Sideboard;
+    }
+
+    public class ShandalarDeckModel
+    {
+        public string Name;
+        public DeckItem[] Core;
+        public DeckItem[] DefaultExtension;
+        public DeckItem[] BlackExtension;
+        public DeckItem[] BlueExtension;
+        public DeckItem[] GreenExtension;
+        public DeckItem[] RedExtension;
+        public DeckItem[] WhiteExtension;
+    }
+}
